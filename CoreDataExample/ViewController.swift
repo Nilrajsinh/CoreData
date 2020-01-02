516ee0c269f3e024d8117737c52d348524028670
Nilrajsinh <Nilrajsinh1@icloud.com>
2020-01-02 09:51:11 +0530

    Initial Commit

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Nilrajsinh Vaghela on 02/01/20.
+//  Copyright © 2020 Nilrajsinh Vaghela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
